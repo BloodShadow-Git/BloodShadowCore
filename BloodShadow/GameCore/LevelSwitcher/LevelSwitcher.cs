40574ae0af95d25e2490fa246e9bab1248bd6b20
Lucifer <64275869+artem-python@users.noreply.github.com>
2025-05-08 18:34:04 +0300

    Fix

--- BloodShadow/GameCore/LevelSwitcher/LevelSwitcher.cs
+++ BloodShadow/GameCore/LevelSwitcher/LevelSwitcher.cs
@@ -20,13 +20,13 @@ namespace BloodShadow.GameCore.LevelSwitcher
         public void Load(string level)
             => Load(level, DEFAULTLOADMODE);
         public void AsyncLoad(string level, out Operation operation)
-            => AsyncLoad(level, out operation, DEFAULTALLOWLEVELACTIVATION, DEFAULTLOADMODE, []);
+            => AsyncLoad(level, out operation, DEFAULTALLOWLEVELACTIVATION, DEFAULTLOADMODE, Array.Empty<Operation>());
         public void AsyncLoad(string level, out Operation operation, bool allowLevelActivation)
-            => AsyncLoad(level, out operation, allowLevelActivation, DEFAULTLOADMODE, []);
+            => AsyncLoad(level, out operation, allowLevelActivation, DEFAULTLOADMODE, Array.Empty<Operation>());
         public void AsyncLoad(string level, out Operation operation, LoadLevelMode mode)
-            => AsyncLoad(level, out operation, DEFAULTALLOWLEVELACTIVATION, mode, []);
+            => AsyncLoad(level, out operation, DEFAULTALLOWLEVELACTIVATION, mode, Array.Empty<Operation>());
         public void AsyncLoad(string level, out Operation operation, bool allowLevelActivation, LoadLevelMode mode)
-            => AsyncLoad(level, out operation, allowLevelActivation, mode, []);
+            => AsyncLoad(level, out operation, allowLevelActivation, mode, Array.Empty<Operation>());
         public void AsyncLoad(string level, out Operation operation, bool allowLevelActivation, params Operation[] additiveTasks)
             => AsyncLoad(level, out operation, allowLevelActivation, DEFAULTLOADMODE, additiveTasks);
         public void AsyncLoad(string level, out Operation operation, LoadLevelMode mode, params Operation[] additiveTasks)
@@ -34,11 +34,11 @@ namespace BloodShadow.GameCore.LevelSwitcher
         public void AsyncLoad(string level, out Operation operation, params Operation[] additiveTasks)
             => AsyncLoad(level, out operation, DEFAULTALLOWLEVELACTIVATION, DEFAULTLOADMODE, additiveTasks);
         public void ForeachAsyncLoad(string[] levels, out Operation operation)
-            => ForeachAsyncLoad(levels, out operation, DEFAULTALLOWLEVELACTIVATION, []);
+            => ForeachAsyncLoad(levels, out operation, DEFAULTALLOWLEVELACTIVATION, Array.Empty<Operation>());
         public void ForeachAsyncLoad(string[] levels, out Operation operation, params Operation[] additiveTasks)
             => ForeachAsyncLoad(levels, out operation, DEFAULTALLOWLEVELACTIVATION, additiveTasks);
         public void ForeachAsyncLoad(string[] levels, out Operation operation, bool allowLevelActivation)
-            => ForeachAsyncLoad(levels, out operation, allowLevelActivation, []);
+            => ForeachAsyncLoad(levels, out operation, allowLevelActivation, Array.Empty<Operation>());
 
         public abstract void Load(string level, LoadLevelMode mode);
         public abstract void AsyncLoad(string level, out Operation operation, bool allowLevelActivation, LoadLevelMode mode, params Operation[] additiveTasks);
